0065b540e1e9194d82c809b71b6fd68702c50dbc
Matteo Ambruschi <matteoambruschi10@gmail.com>
2023-01-20 18:20:59 +0100

    mouse

--- script.js
+++ script.js
@@ -35,3 +35,29 @@ LogoUp.addEventListener('click', function() {
     console.log('prova clic pulsante')
     window.scrollTo(0,0);
 });
+
+
+let outline = document.querySelector('.outline');
+let cursor = document.querySelector('.cursor');
+let links = document.querySelectorAll('a');
+
+document.addEventListener('mousemove', function(e){
+
+  let x = e.clientX;
+  let y = e.clientY;
+
+  outline.style.transform = `translate( calc(${x}px - 50%), calc(${y}px - 50%) )`;
+  cursor.style.transform = `translate( calc(${x}px - 50%), calc(${y}px - 50%) )`;
+});
+
+links.forEach((link) => {
+  link.addEventListener("mouseover", function() {
+    outline.classList.add('hover');
+    cursor.classList.add('hover');
+  });
+  
+  link.addEventListener("mouseleave", function() {
+    outline.classList.remove('hover');
+    cursor.classList.remove('hover');
+  });
+});
