bf62adcaaefc1c4012f612e5772e84fe627685cc
Matteo Ambruschi <121877088+MatteoAmbruschi@users.noreply.github.com>
2023-04-06 15:40:30 +0200

    prova

--- script.js
+++ script.js
@@ -136,6 +136,7 @@ myButton.addEventListener('click', () => {
 //////GSAP
 
 gsap.to(".hero", { opacity: 1, duration: 2.5,});
+gsap.from(".hero-cta", { opacity: 0, duration: 1, y: 20, stagger: 0.2});
 
 
 const button = document.querySelector('.button');
