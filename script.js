aaf54d3c076dfeef76b3bf8e80746a0591c56528
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-03-31 22:56:12 +0200

    modfiche animaziono: aggiunta libreria caratteri e animazioni titoli, animazione testi in opacità, aggiunta animazione numeri in other works, iubenda chimato in scroll

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger)
+
 // freccia torna su
 const backBtn = document.getElementById('backBtn');
 
@@ -6,13 +8,12 @@ window.addEventListener('scroll', function() {
 
     if(posY > 600) {
         backBtn.style.opacity = '1';
-        backBtn.style.cursor = 'pointer';
+        backBtn.style.zIndex = '1'
     } else {
         backBtn.style.opacity = '0';
-        backBtn.style.cursor = 'default';
+        backBtn.style.zIndex = '-1'
     }
 });
-
 
 
 
@@ -26,7 +27,11 @@ document.addEventListener('mousemove', ({ clientX: x, clientY: y }) => {
   cursor.style.transform = translateValue;
 });
 
-const links = document.querySelectorAll('a');
+let buttonlink = document.querySelectorAll('button');
+let a = document.querySelectorAll('a');
+let logoImg = document.querySelectorAll('.logoImg');
+
+let links = [...buttonlink, ...a, ...logoImg, backBtn];
 const handleLinkHover = (event) => {
   outline.classList.toggle('hover', event.type === 'mouseover');
   cursor.classList.toggle('hover', event.type === 'mouseover');
@@ -39,21 +44,7 @@ links.forEach((link) => {
 
 
 
-/* 
-
-// Pulsante coockie
-const myButton = document.querySelector('#myButton');
-
-myButton.addEventListener('click', () => {
-  window.location.replace('cookie.html');
-});
-
- */
-
-
-
 //////GSAP
-
 gsap.to(".hero", { opacity: 1, duration: 2.5,});
 gsap.to(".hero-cta", { opacity: 1, duration: 1, y: 20, delay: 0.2,});
 
@@ -62,8 +53,7 @@ gsap.to(".hero-cta", { opacity: 1, duration: 1, y: 20, delay: 0.2,});
 if(window.innerWidth > 769){
 
   document.addEventListener("DOMContentLoaded", () => {
-    gsap.registerPlugin(ScrollTrigger);
-  
+
     function handleScrollEvent(id) {
       gsap.to(id, {
         fontSize: '12px',
@@ -85,30 +75,29 @@ if(window.innerWidth > 769){
   });
 }
 
-
-const button = document.querySelector('.button');
-{const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      gsap.to('.button', { opacity: 1, duration: 1.3, stagger: 0.3 });
-      observer.unobserve(entry.target);
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector('.button');
+  {const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        gsap.to('.button', { opacity: 1, duration: 1.3, stagger: 0.3 });
+        observer.unobserve(entry.target);
+      }
+    });
   });
-});
-observer.observe(button);}
-
+  observer.observe(button);}
+})
 
 
 //Lavori
 const watchElements  = document.querySelectorAll('.watch')
 
 watchElements.forEach((watch) => {
-  document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger)
-      gsap.to(watch, {
-        opacity: 1,
-        scale: 1,
-        filter: 'blur(0px)',
+  document.addEventListener("DOMContentLoaded", () => {
+      gsap.from(watch, {
+        opacity: 0.2,
+        scale: 0.8,
+        filter: 'blur(3px)',
         scrollTrigger: {
           scrub: 1,
           trigger: watch,
@@ -120,7 +109,6 @@ watchElements.forEach((watch) => {
 })
 
 
-
 //lenis locomotive scroll
 const lenis = new Lenis()
 function raf(time) {
@@ -128,3 +116,80 @@ function raf(time) {
   requestAnimationFrame(raf)
 }
 requestAnimationFrame(raf)
+
+
+// Funzione per generare lettere casuali
+const randomLettersContainer = document.querySelector(".random-letters");
+
+function generateRandomLetters() {
+    const letters = "123456789";
+    let randomText = "";
+
+    for (let i = 0; i < 2; i++) {
+        const randomIndex = Math.floor(Math.random() * letters.length);
+        randomText += letters.charAt(randomIndex)
+    }
+
+    return `${randomText}`;
+}
+
+    function update() {
+        const randomText = generateRandomLetters();
+        randomLettersContainer.textContent = randomText;
+        setTimeout(update, 350);
+    }
+    update();
+
+
+
+//EFFETTO TITOLI 
+document.addEventListener("DOMContentLoaded", () => {
+  const splitTypes = document.querySelectorAll('h2')
+  splitTypes.forEach((char, i) => {
+      const text = new SplitType(char, { types: 'chars'})
+
+      gsap.from(text.chars, {
+          scrollTrigger: {
+              trigger: char,
+              start: 'top 90%',
+              end: 'top 23%',
+              scrub: true,
+              markers: false,
+          },
+          opacity: 0.2,
+          stagger: 0.1,
+      })
+  })
+
+    function applyOpacity(opc) {
+      gsap.from(opc, {
+        opacity: 0.2,
+        scrollTrigger: {
+          scrub: true,
+          trigger: opc,
+          markers: false,
+          start: "top 90%",
+          end: "top 50%",
+        }
+      });
+    }
+
+    const texts = document.querySelectorAll('.work-info')
+    const tags = document.querySelectorAll('.works-item-tags')
+    const aboutMe = document.getElementById('aboutMe')
+    const contacts = document.getElementsByClassName('contacts')
+    const containerOpacity = [...texts, ...tags, aboutMe, contacts]
+
+    containerOpacity.forEach((opc) => applyOpacity(opc))
+})
+
+
+const titleBfl = document.getElementById('titleBfl')
+if(window.innerWidth < 470){
+  titleBfl.innerHTML = 'Business For<br> Lawyers'
+}
+
+const titleLarte = document.getElementById('titleLarte')
+if(window.innerWidth < 400){
+  titleLarte.innerHTML = "L'arte<br> D'annodare"
+}
